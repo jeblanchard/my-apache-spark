af504354ef1343313be3facddab24d496bfef2e0
Joshua Blanchard <jeblanchard@berkeley.edu>
2023-05-04 19:50:03 -0700

    refactored project scripts

---
+++ scripts/helper/build.sh
@@ -0,0 +1,5 @@
+docker stop my-apache-spark
+
+docker rm my-apache-spark
+
+docker build -t jeblanchard/personal-website:my-apache-spark .
--- scripts/run-all-integ-tests.sh
+++ scripts/run-all-integ-tests.sh
@@ -1,5 +1,13 @@
 printf "\n"
 
+docker stop my-apache-spark
+
+docker rm my-apache-spark
+
+docker build -t jeblanchard/personal-website:my-apache-spark .
+
+docker run --name my-apache-spark -it jeblanchard/personal-website:my-apache-spark sh
+
 for f in ./test/integration/*
 do
   echo $f
